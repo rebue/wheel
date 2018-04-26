bbffc164359e6b5bb455944082699483a983f5ea
zbz <nnzbz@163.com>
2018-04-26 11:01:52 +0800

    + SignUtils增加通用签名方法

--- wheel-turing/src/main/java/rebue/wheel/turing/SignUtils.java
+++ wheel-turing/src/main/java/rebue/wheel/turing/SignUtils.java
@@ -5,53 +5,117 @@ import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 签名的相关应用
  */
 public class SignUtils {
+    private final static Logger _log = LoggerFactory.getLogger(SignUtils.class);
+
     /**
-     * 签名2-微信签名
+     * 签名-签名
      * 在请求时，通过签名算法算出签名，并将其放入请求的Map中
-     * 签名算法: 将所有参数排序生成新的Map-》拼接成key1=value2&amp;key2=value2....的字符串-》md5hex-》大写
+     * 签名算法步骤:
+     * 1. 将所有参数排序生成新的Map
+     * 2. 参数列表添加key后，一起拼接成name1=value1&name2=value2....&key=xxx的字符串
+     * 3. 将字符串md5hex并大写生成签名
+     * 4. 将生成的签名添加入参数map中
+     * 
+     * @param requestParams
+     *            请求的参数map
+     * @param signKeyName
+     *            拼接到键值对字符串后的签名key的名称
+     * @param signKeyValue
+     *            拼接到键值对字符串后的签名key的值
+     * @param signParamName
+     *            将生成的签名添加入参数map中的名称
      */
-    public static void sign2(Map<String, String> requestParams, String signKey) {
-        // 排序生成新的Map
-        Map<String, String> sortMap = new TreeMap<>(requestParams);
-        // 拼接成key1=value2&key2=value2....的字符串
+    public static void sign(Map<String, Object> requestParams, String signKeyName, String signKeyValue, String signParamName) {
+        _log.info("计算签名: requestParams={},signKeyName={},signKeyValue={},signParamName={}", requestParams, signKeyName, signKeyValue, signParamName);
+        // 1. 将所有参数排序生成新的Map
+        Map<String, Object> sortMap = new TreeMap<>(requestParams);
+        // 2. 参数列表添加key一起拼接成name1=value1&name2=value2....&key=xxx的字符串
         StringBuilder sb = new StringBuilder();
-        for (Entry<String, String> item : sortMap.entrySet()) {
-            if (StringUtils.isBlank(item.getValue()))
+        for (Entry<String, Object> item : sortMap.entrySet()) {
+            if (StringUtils.isBlank((String) item.getValue()))
                 continue;
             if (sb.length() > 0)
                 sb.append("&");
             sb.append(item.getKey() + "=" + item.getValue());
         }
-        sb.append("&key=" + signKey);
-        // md5hex->大写->放入请求的Map中
-        requestParams.put("sign", DigestUtils.md5AsHexStr(sb.toString().getBytes()).toUpperCase());
+        sb.append("&" + signKeyName + "=" + signKeyValue);
+        // 3. 将字符串md5hex并大写生成签名
+        String sign = DigestUtils.md5AsHexStr(sb.toString().getBytes()).toUpperCase();
+        _log.info("生成签名: {}", sign);
+        // 4. 将生成的签名添加入参数map中
+        requestParams.put(signParamName, sign);
     }
 
     /**
-     * 签名2-校验微信签名
+     * 签名-验证签名
      * 接收到的请求时，对请求的参数进行签名校验
+     * 
+     * @param requestParams
+     *            接收到的请求参数map
+     * @param signKeyName
+     *            拼接到键值对字符串后的签名key的名称
+     * @param signKeyValue
+     *            拼接到键值对字符串后的签名key的值
+     * @param signParamName
+     *            将生成的签名添加入参数map中的名称
      */
-    public static boolean verify2(Map<String, String> requestParams, String signKey) {
+    public static boolean verify(Map<String, Object> requestParams, String signKeyName, String signKeyValue, String signParamName) {
+        _log.info("验证签名: requestParams={},signKeyName={},signKeyValue={},signParamName={}", requestParams, signKeyName, signKeyValue, signParamName);
         StringBuilder sb = new StringBuilder();
-        Map<String, String> sortMap = new TreeMap<>(requestParams);
+        Map<String, Object> sortMap = new TreeMap<>(requestParams);
 
-        for (Entry<String, String> item : sortMap.entrySet()) {
-            if (StringUtils.isBlank(item.getValue()))
+        for (Entry<String, Object> item : sortMap.entrySet()) {
+            if (StringUtils.isBlank((String) item.getValue()))
                 continue;
-            if ("sign".equalsIgnoreCase(item.getKey())) {
+            if (signParamName.equalsIgnoreCase(item.getKey())) {
                 continue;
             }
             if (sb.length() > 0)
                 sb.append("&");
             sb.append(item.getKey() + "=" + item.getValue());
         }
-        sb.append("&key=" + signKey);
+        sb.append("&" + signKeyName + "=" + signKeyValue);
         String sign = DigestUtils.md5AsHexStr(sb.toString().getBytes()).toUpperCase();
-        return sign.equalsIgnoreCase(sortMap.get("sign"));
+        _log.info("生成签名: {}", sign);
+        return sign.equalsIgnoreCase((String) sortMap.get(signParamName));
+    }
+
+    /**
+     * 签名2-通用签名
+     * 在请求时，通过签名算法算出签名，并将其放入请求的Map中
+     */
+    public static void sign1(Map<String, Object> requestParams, String signKeyValue) {
+        sign(requestParams, "SignKeyName", signKeyValue, "SignParamName");
+    }
+
+    /**
+     * 签名2-验证通用签名
+     * 接收到的请求时，对请求的参数进行签名验证
+     */
+    public static boolean verify1(Map<String, Object> requestParams, String signKeyValue) {
+        return verify(requestParams, "SignKeyName", signKeyValue, "SignParamName");
+    }
+
+    /**
+     * 签名2-微信支付签名
+     * 在请求时，通过签名算法算出签名，并将其放入请求的Map中
+     */
+    public static void sign2(Map<String, Object> requestParams, String signKeyValue) {
+        sign(requestParams, "key", signKeyValue, "sign");
+    }
+
+    /**
+     * 签名2-验证微信支付签名
+     * 接收到的请求时，对请求的参数进行签名验证
+     */
+    public static boolean verify2(Map<String, Object> requestParams, String signKeyValue) {
+        return verify(requestParams, "key", signKeyValue, "sign");
     }
 }
