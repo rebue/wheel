20cc94df88be776a401da0b43400d0d040fe2eff
zbz <nnzbz@qq.com>
2018-11-28 17:27:18 +0800

    * 补充测试BigDecimal

--- wheel-core/src/test/java/rebue/wheel/BigDecimalTests.java
+++ wheel-core/src/test/java/rebue/wheel/BigDecimalTests.java
@@ -5,6 +5,9 @@ import java.math.BigDecimal;
 import org.junit.Test;
 
 public class BigDecimalTests {
+    /**
+     * intValue只会截取整数部分，需要先setScale四舍五入
+     */
     @Test
     public void test01() {
         final BigDecimal one = BigDecimal.valueOf(1.11);
@@ -13,5 +16,11 @@ public class BigDecimalTests {
         final BigDecimal two = BigDecimal.valueOf(0.05);
         System.out.println(two.intValue());
         System.out.println(two.multiply(BigDecimal.valueOf(60)).intValue());
+        final BigDecimal three = BigDecimal.valueOf(1.33);
+        System.out.println(three.intValue());
+        System.out.println(three.multiply(BigDecimal.valueOf(60)).intValue());
+        final BigDecimal four = BigDecimal.valueOf(1.33);
+        System.out.println(four.intValue());
+        System.out.println(four.multiply(BigDecimal.valueOf(60)).setScale(0, BigDecimal.ROUND_HALF_UP).intValue());
     }
 }
