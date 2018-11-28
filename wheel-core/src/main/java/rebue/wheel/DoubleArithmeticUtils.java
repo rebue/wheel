1284ccd5d03ea9d3d32d333fc1f5eca8af598e2d
zbz <nnzbz@qq.com>
2018-11-28 18:58:41 +0800

    * 补充BigDecimal比较是否相等的测试

--- wheel-core/src/main/java/rebue/wheel/DoubleArithmeticUtils.java
+++ wheel-core/src/main/java/rebue/wheel/DoubleArithmeticUtils.java
@@ -11,77 +11,87 @@ import java.math.BigDecimal;
  */
 public class DoubleArithmeticUtils {
     /**
+     * 判断是否相等(应该用compareTo)。
+     * equals方法会比较值和精确度，而compareTo则会忽略精度。
+     */
+    public static boolean equals(final Double value1, final Double value2) {
+        final BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        return b1.compareTo(b2) == 0;
+    }
+
+    /**
      * 提供精确的加法运算。
      */
-    public static double add(Double value1, Double value2) {
-        BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
-        BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+    public static double add(final Double value1, final Double value2) {
+        final BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
         return b1.add(b2).doubleValue();
     }
 
     /**
      * 提供(相对)精确的加法运算，精确到小数点以后scale位，以后的数字四舍五入。
      */
-    public static double add(Double value1, Double value2, int scale) {
-        BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
-        BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+    public static double add(final Double value1, final Double value2, final int scale) {
+        final BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
         return b1.add(b2).setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
     /**
      * 提供精确的减法运算。
      */
-    public static double sub(Double value1, Double value2) {
-        BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
-        BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+    public static double sub(final Double value1, final Double value2) {
+        final BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
         return b1.subtract(b2).doubleValue();
     }
 
     /**
      * 提供(相对)精确的减法运算，精确到小数点以后scale位，以后的数字四舍五入。
      */
-    public static double sub(Double value1, Double value2, int scale) {
-        BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
-        BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+    public static double sub(final Double value1, final Double value2, final int scale) {
+        final BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
         return b1.subtract(b2).setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
     /**
      * 提供精确的乘法运算。
      */
-    public static double mul(Double value1, Double value2) {
-        BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
-        BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+    public static double mul(final Double value1, final Double value2) {
+        final BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
         return b1.multiply(b2).doubleValue();
     }
 
     /**
      * 提供(相对)精确的乘法运算，精确到小数点以后scale位，以后的数字四舍五入。
      */
-    public static double mul(Double value1, Double value2, int scale) {
-        BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
-        BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+    public static double mul(final Double value1, final Double value2, final int scale) {
+        final BigDecimal b1 = BigDecimal.valueOf(value1);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(value2);    // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
         return b1.multiply(b2).setScale(scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
     /**
      * 提供精确的除法运算
      */
-    public static Double divide(Double dividend, Double divisor) {
-        BigDecimal b1 = BigDecimal.valueOf(dividend);  // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
-        BigDecimal b2 = BigDecimal.valueOf(divisor);   // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+    public static Double divide(final Double dividend, final Double divisor) {
+        final BigDecimal b1 = BigDecimal.valueOf(dividend);  // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(divisor);   // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
         return b1.divide(b2).doubleValue();
     }
 
     /**
      * 提供（相对）精确的除法运算，精确到小数点以后scale位，以后的数字四舍五入。
      */
-    public static Double divide(Double dividend, Double divisor, int scale) {
+    public static Double divide(final Double dividend, final Double divisor, final int scale) {
         if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
-        BigDecimal b1 = BigDecimal.valueOf(dividend);  // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
-        BigDecimal b2 = BigDecimal.valueOf(divisor);   // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b1 = BigDecimal.valueOf(dividend);  // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
+        final BigDecimal b2 = BigDecimal.valueOf(divisor);   // 注意: 在构造Double类型的BigDecimal时不能直接用new BigDecimal(Double)的方法
         return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
--- wheel-core/src/test/java/rebue/wheel/BigDecimalTests.java
+++ wheel-core/src/test/java/rebue/wheel/BigDecimalTests.java
@@ -2,6 +2,7 @@ package rebue.wheel;
 
 import java.math.BigDecimal;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class BigDecimalTests {
@@ -22,5 +23,25 @@ public class BigDecimalTests {
         final BigDecimal four = BigDecimal.valueOf(1.33);
         System.out.println(four.intValue());
         System.out.println(four.multiply(BigDecimal.valueOf(60)).setScale(0, BigDecimal.ROUND_HALF_UP).intValue());
+    }
+
+    /**
+     * equals方法会比较值和精确度，而compareTo则会忽略精度。
+     */
+    @Test
+    public void test02() {
+        BigDecimal one = BigDecimal.valueOf(11);
+        BigDecimal two = BigDecimal.valueOf(11.00);
+        Assert.assertNotEquals(one, two);
+        Assert.assertNotEquals(true, one.equals(two));
+        one = BigDecimal.valueOf(223.0).add(BigDecimal.ZERO);
+        two = BigDecimal.valueOf(223.0000).add(BigDecimal.ZERO);
+        Assert.assertEquals(one, two);
+        Assert.assertEquals(true, one.equals(two));
+
+        final BigDecimal x = new BigDecimal("1");
+        final BigDecimal y = new BigDecimal("1.00");
+        System.out.println(x.equals(y));
+        System.out.println(x.compareTo(y) == 0 ? "true" : "false");
     }
 }
