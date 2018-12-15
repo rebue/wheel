2dd85c0ae23394111678aea79a57364f58aa65d9
zbz <nnzbz@qq.com>
2018-12-15 11:30:14 +0800

    + 生成随机汉字

--- wheel-core/src/test/java/rebue/wheel/RandomExTest.java
+++ wheel-core/src/test/java/rebue/wheel/RandomExTest.java
@@ -9,33 +9,51 @@ import java.util.Random;
 
 import org.junit.Test;
 
+import entity.PojoEntity;
 import rebue.wheel.test.AbstractSpringContextTest;
 
 public class RandomExTest extends AbstractSpringContextTest {
 
-	@Test
-	public void test01() throws NoSuchAlgorithmException, NoSuchProviderException {
-		Random random;
-		long start = System.currentTimeMillis();
-		List<Integer> randoms = new ArrayList<Integer>();
-		int id;
-		for (int i = 0; i < 10; i++) {
-			random = RandomEx.getRandom();
-			id = random.nextInt(Integer.MAX_VALUE);
-			// System.out.println(random.nextInt(Integer.MAX_VALUE));
-			randoms.add(id);
-		}
-		long end = System.currentTimeMillis();
-		System.out.println(String.format("耗时%d毫秒", end - start));
-		printDistribution("Random", randoms);
-	}// test01
+//	@Test
+    public void test01() throws NoSuchAlgorithmException, NoSuchProviderException {
+        Random random;
+        final long start = System.currentTimeMillis();
+        final List<Integer> randoms = new ArrayList<>();
+        int id;
+        for (int i = 0; i < 10; i++) {
+            random = RandomEx.getRandom();
+            id = random.nextInt(Integer.MAX_VALUE);
+            // System.out.println(random.nextInt(Integer.MAX_VALUE));
+            randoms.add(id);
+        }
+        final long end = System.currentTimeMillis();
+        System.out.println(String.format("耗时%d毫秒", end - start));
+        printDistribution("Random", randoms);
+    }// test01
 
-	private void printDistribution(String name, List<Integer> frequencies) {
-		System.out.printf("%n%s distribution |8000     |9000     |10000    |11000    |12000%n", name);
-		for (int i = 0; i < 10; i++) {
-			char[] bar = "                                                  ".toCharArray(); // 50 chars.
-			Arrays.fill(bar, 0, Math.max(0, Math.min(50, frequencies.get(i) / 100 - 80)), '#');
-			System.out.printf("0.%dxxx: %6d  :%s%n", i, frequencies.get(i), new String(bar));
-		}
-	}
+    private void printDistribution(final String name, final List<Integer> frequencies) {
+        System.out.printf("%n%s distribution |8000     |9000     |10000    |11000    |12000%n", name);
+        for (int i = 0; i < 10; i++) {
+            final char[] bar = "                                                  ".toCharArray(); // 50 chars.
+            Arrays.fill(bar, 0, Math.max(0, Math.min(50, frequencies.get(i) / 100 - 80)), '#');
+            System.out.printf("0.%dxxx: %6d  :%s%n", i, frequencies.get(i), new String(bar));
+        }
+    }
+
+    /**
+     * 测试随机生成汉字
+     */
+    @Test
+    public void testRandomCn() {
+        System.out.println(RandomEx.randomCnChar());
+        System.out.println(RandomEx.randomCnStr(1000));
+    }
+
+    /**
+     * 测试生成随机属性值的对象
+     */
+    @Test
+    public void testRandomPojo() throws ReflectiveOperationException {
+        System.out.println(RandomEx.randomPojo(new PojoEntity().getClass()));
+    }
 }
