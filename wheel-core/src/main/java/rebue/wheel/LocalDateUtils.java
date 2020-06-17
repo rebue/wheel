00e58c48e9bab4e20c293525b7d8abecb26fcf08
zbz <nnzbz@qq.com>
2020-06-17 11:50:40 +0800

    + 添加LocalDateUtils

---
+++ wheel-core/src/main/java/rebue/wheel/LocalDateUtils.java
@@ -0,0 +1,30 @@
+package rebue.wheel;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+public class LocalDateUtils {
+
+    /**
+     * Date转换为LocalDateTime
+     */
+    public static LocalDateTime date2LocalDateTime(final Date date) {
+        final Instant instant = date.toInstant();// An instantaneous point on the time-line.(时间线上的一个瞬时点。)
+        final ZoneId  zoneId  = ZoneId.systemDefault();// A time-zone ID, such as {@code Europe/Paris}.(时区)
+        return instant.atZone(zoneId).toLocalDateTime();
+    }
+
+    /**
+     * LocalDateTime转换为Date
+     */
+    public static Date localDateTime2Date(final LocalDateTime localDateTime) {
+        final ZoneId        zoneId = ZoneId.systemDefault();
+        final ZonedDateTime zdt    = localDateTime.atZone(zoneId);// Combines this date-time with a time-zone to create a ZonedDateTime.
+        return Date.from(zdt.toInstant());
+
+    }
+
+}
