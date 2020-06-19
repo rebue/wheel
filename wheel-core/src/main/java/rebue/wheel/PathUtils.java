9f9c0b397a5e845822c5c436fc5c52a82aac653a
zbz <nnzbz@qq.com>
2020-06-19 19:55:09 +0800

    + 添加PathUtils

---
+++ wheel-core/src/main/java/rebue/wheel/PathUtils.java
@@ -0,0 +1,15 @@
+package rebue.wheel;
+
+import java.nio.file.FileSystems;
+
+public class PathUtils {
+    /**
+     * 判断是否是绝对路径
+     *
+     * @param path 需要判断的路径
+     * @return 是否是绝对路径，如果是false，则说明是相对路径
+     */
+    public final static boolean isAbsPath(String path) {
+        return FileSystems.getDefault().getPath(path).isAbsolute();
+    }
+}
