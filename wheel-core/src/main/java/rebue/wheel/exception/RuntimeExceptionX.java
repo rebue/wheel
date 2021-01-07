e510da8566dd135bd98bf0ec4504678ad8bc8ba6
zbz <nnzbz@21cn.com>
2021-01-07 18:26:41 +0800

    * 优化代码

--- wheel-core/src/main/java/rebue/wheel/exception/RuntimeExceptionX.java
+++ wheel-core/src/main/java/rebue/wheel/exception/RuntimeExceptionX.java
@@ -1,25 +1,23 @@
 package rebue.wheel.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 扩展RuntimeException，详细记录异常日志
  */
+@Slf4j
 public class RuntimeExceptionX extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    private static Logger     _log             = LoggerFactory.getLogger(RuntimeExceptionX.class);
-
     public RuntimeExceptionX(final String msg) {
         super(msg);
-        _log.error(msg);
+        log.error(msg);
     }
 
     public RuntimeExceptionX(final String msg, final Throwable t) {
         super(msg, t);
-        _log.error(msg, t);
+        log.error(msg, t);
     }
 
 }
