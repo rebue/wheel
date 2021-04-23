cd1e28d1f9539f8bf1cc86a9cbbf7efd93458d18
zbz <nnzbz@qq.com>
2021-04-23 16:34:19 +0800

    * 修正Kryo序列化与反序列化的BUG

--- wheel-serialization/src/main/java/rebue/wheel/serialization/kryo/KryoUtils.java
+++ wheel-serialization/src/main/java/rebue/wheel/serialization/kryo/KryoUtils.java
@@ -1,5 +1,9 @@
 package rebue.wheel.serialization.kryo;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
@@ -11,27 +15,23 @@ public class KryoUtils {
         if (obj == null) {
             return null;
         }
-        try (final Output output = new Output()) {
+        try (final ByteArrayOutputStream baos = new ByteArrayOutputStream(); final Output output = new Output(baos)) {
             kryos.get().writeClassAndObject(output, obj);
-            return output.toBytes();
+            output.flush();
+            return baos.toByteArray();
+        } catch (final IOException e) {
+            throw new RuntimeException("Kryo序列化出现异常", e);
         }
-
-        // try (final ByteArrayOutputStream baos = new ByteArrayOutputStream(); final Output output = new Output(baos)) {
-        // _kryo.writeClassAndObject(output, obj);
-        // output.flush();
-        // return baos.toByteArray();
-        // }
     }
 
     public static Object readObject(final byte[] bytes) {
         if (bytes == null || bytes.length == 0) {
             return null;
         }
-        try (Input input = new Input()) {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes); Input input = new Input(bais)) {
             return kryos.get().readClassAndObject(input);
+        } catch (final IOException e) {
+            throw new RuntimeException("Kryo反序列化出现异常", e);
         }
-        // try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes); Input input = new Input(bais)) {
-        // return _kryo.readClassAndObject(input);
-        // }
     }
 }
