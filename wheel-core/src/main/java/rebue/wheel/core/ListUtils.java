acda00e283a91ea68ff6af36df5e62c48601714a
zbz <nnzbz@qq.com>
2021-04-29 21:12:03 +0800

    * 修改List转String的实现

--- wheel-core/src/main/java/rebue/wheel/core/ListUtils.java
+++ wheel-core/src/main/java/rebue/wheel/core/ListUtils.java
@@ -1,18 +1,20 @@
 package rebue.wheel.core;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListUtils {
     /**
      * 将List转成item1,item2,item3....的字符串格式
      */
-    public static String toString(List<?> list) {
-        String result = "";
-        for (Object object : list) {
-            result += object + ",";
-        }
-        if (result.length() > 0)
-            result = StrUtils.delRight(result, 1);
-        return result;
+    public static String toString(final List<?> list) {
+        // String result = "";
+        // for (Object object : list) {
+        // result += object + ",";
+        // }
+        // if (result.length() > 0)
+        // result = StrUtils.delRight(result, 1);
+        // return result;
+        return list.stream().map(Object::toString).collect(Collectors.joining(","));
     }
 }
