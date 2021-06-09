f469bff4ec1387e84c4a98162aa47fe63b6b773e
nnzbz <nnzbz@qq.com>
2021-06-09 17:33:05 +0800

    + Base64Utils添加encodeUrl方法

--- wheel-turing/src/main/java/rebue/wheel/turing/Base64Utils.java
+++ wheel-turing/src/main/java/rebue/wheel/turing/Base64Utils.java
@@ -1,30 +1,37 @@
 package rebue.wheel.turing;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.Base64;
 
 /**
  * @author zbz
+ *
  * @since 1.8
  */
 public class Base64Utils {
-    public static byte[] encode(byte[] data) throws IOException {
+    public static byte[] encode(final byte[] data) throws IOException {
         return Base64.getEncoder().encode(data);
     }
 
-    public static byte[] decode(String sData) throws IOException {
+    public static byte[] decode(final String sData) throws IOException {
         return Base64.getDecoder().decode(sData);
     }
 
-    public static byte[] decode(byte[] data) throws IOException {
+    public static byte[] decode(final byte[] data) throws IOException {
         return Base64.getDecoder().decode(data);
     }
 
-    public static String encodeStr(byte[] data) throws IOException {
+    public static String encodeStr(final byte[] data) throws IOException {
         return new String(encode(data));
     }
 
-    public static String decodeStr(String sData) throws IOException {
+    public static String decodeStr(final String sData) throws IOException {
         return new String(decode(sData));
     }
+
+    public static String encodeUrl(final byte[] data) throws IOException {
+        return URLEncoder.encode(new String(encode(data)), "utf-8");
+    }
+
 }
