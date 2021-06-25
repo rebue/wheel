1696999e040d009e84a7f2992ebee63edfdce24b
nnzbz <nnzbz@qq.com>
2021-06-25 10:28:17 +0800

    * 优化OrikaUtils

--- wheel-api/src/main/java/rebue/wheel/api/OrikaUtils.java
+++ wheel-api/src/main/java/rebue/wheel/api/OrikaUtils.java
@@ -1,5 +1,7 @@
 package rebue.wheel.api;
 
+import java.util.List;
+
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 
@@ -7,6 +9,33 @@ public class OrikaUtils {
     /**
      * 克隆工具，可以进行复杂和深度的克隆
      */
-    public static final MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+    public final static MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
 
+    /**
+     * 克隆对象
+     *
+     * @param <S>              源对象类型
+     * @param <D>              目的对象类型
+     * @param sourceObject     源对象
+     * @param destinationClass 目的对象类型
+     *
+     * @return
+     */
+    public final static <S, D> D map(final S sourceObject, final Class<D> destinationClass) {
+        return OrikaUtils.mapperFactory.getMapperFacade().map(sourceObject, destinationClass);
+    }
+
+    /**
+     * 克隆List
+     *
+     * @param <S>              源List元素类型
+     * @param <D>              目的List元素类型
+     * @param source           源对象
+     * @param destinationClass 目的List元素类型
+     *
+     * @return
+     */
+    public final static <S, D> List<D> mapAsList(final Iterable<S> source, final Class<D> destinationClass) {
+        return OrikaUtils.mapperFactory.getMapperFacade().mapAsList(source, destinationClass);
+    }
 }
