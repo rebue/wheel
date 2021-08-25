6e034d76bc9dd4bea284ed9b65e8c43a4e87f399
lq <1213811243@qq.com>
2021-08-25 11:35:47 +0800

    修改jwt庫

--- wheel-turing/src/main/java/rebue/wheel/turing/JwtUtils.java
+++ wheel-turing/src/main/java/rebue/wheel/turing/JwtUtils.java
@@ -2,12 +2,8 @@ package rebue.wheel.turing;
 
 import java.text.ParseException;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpCookie;
@@ -16,14 +12,6 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.util.MultiValueMap;
 
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSSigner;
-import com.nimbusds.jose.JWSVerifier;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jose.crypto.MACVerifier;
-import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 
 import lombok.extern.slf4j.Slf4j;
@@ -31,78 +19,19 @@ import rebue.wheel.core.LocalDateTimeUtils;
 
 @Slf4j
 public class JwtUtils {
+
     public final static String JWT_TOKEN_NAME = "jwt_token";
 
     /**
-     * JWT签名
-     *
-     * @param key
-     *                  签名的密钥
-     * @param claimsSet
-     *                  JWT中payload部分的内容
-     *
-     * @throws JOSEException
-     *                       签名失败
-     */
-    public static String sign(final byte[] key, final JWTClaimsSet claimsSet) throws JOSEException {
-        log.info("开始计算JWT签名");
-
-        if (key.length < 64) {
-            throw new IllegalArgumentException("密钥的字节长度不能小于64个Byte，目前是" + key.length + "个Byte");
-        }
-
-        // Prepare JWT with claims set
-        final SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS512), claimsSet);
-
-        // Create HMAC signer
-        final JWSSigner signer = new MACSigner(key);
-
-        // Apply the HMAC protection
-        signedJWT.sign(signer);
-
-        // Serialize to compact form, produces something like
-        // eyJhbGciOiJIUzI1NiJ9.SGVsbG8sIHdvcmxkIQ.onO9Ihudz3WkiauDO2Uhyuz0Y18UASXlSc1eS0NkWyA
-        final String sign = signedJWT.serialize();
-        final String msg  = "JWT签名成功";
-        log.info("{}: {}", msg, sign);
-        return sign;
-    }
-
-    /**
      * 将JWT签名添加到Cookie中
      *
-     * @param sign
-     *                       JWT的签名
-     * @param expirationTime
-     *                       JWT签名的过期时间
+     * @param sign           JWT的签名
+     * @param expirationTime JWT签名的过期时间
      */
-    public static void addCookie(final String sign, final Date expirationTime, final ServerHttpResponse resp) {
-        addCookie(sign, expirationTime.getTime(), resp);
-    }
-
-    /**
-     * 将JWT签名添加到Cookie中
-     *
-     * @param sign
-     *                       JWT的签名
-     * @param expirationTime
-     *                       JWT签名的过期时间
-     */
-    public static void addCookie(final String sign, final LocalDateTime expirationTime, final ServerHttpResponse resp) {
-        addCookie(sign, LocalDateTimeUtils.getMillis(expirationTime), resp);
-    }
-
-    /**
-     * 将JWT签名添加到Cookie中
-     *
-     * @param sign
-     *                       JWT的签名
-     * @param expirationTime
-     *                       JWT签名的过期时间(1970年1月1日零时至此的毫秒数)
-     */
-    public static void addCookie(final String sign, final Long expirationTime, final ServerHttpResponse resp) {
+    public static void addCookie(final String sign, final LocalDateTime expirationTime, final ServerHttpResponse resp)
+    {
         log.info("将JWT签名添加到Cookie中");
-        final ResponseCookie responseCookie = ResponseCookie.from(JWT_TOKEN_NAME, sign).maxAge((int) ((expirationTime - System.currentTimeMillis()) / 1000)).path("/").build();
+        final ResponseCookie responseCookie = ResponseCookie.from(JWT_TOKEN_NAME, sign).maxAge((int) ((LocalDateTimeUtils.getMillis(expirationTime) - System.currentTimeMillis()) / 1000)).path("/").build();
         resp.addCookie(responseCookie);
     }
 
@@ -111,25 +40,8 @@ public class JwtUtils {
      *
      * @return JWT的签名
      */
-    public static String getSignFromCookies(final HttpServletRequest req) {
-        log.info("从请求的Cookie中获取JWT签名信息");
-        final Cookie[] cookies = req.getCookies();
-        if (cookies != null) {
-            for (final Cookie cookie : cookies) {
-                if (JWT_TOKEN_NAME.equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null;
-    }
-
-    /**
-     * 从请求的Cookie中获取JWT签名信息
-     *
-     * @return JWT的签名
-     */
-    public static String getSignFromCookies(final ServerHttpRequest req) {
+    public static String getSignFromCookies(final ServerHttpRequest req)
+    {
         log.info("从请求的Cookie中获取JWT签名信息");
         final MultiValueMap<String, HttpCookie> cookies = req.getCookies();
         if (cookies != null && !cookies.isEmpty()) {
@@ -142,75 +54,15 @@ public class JwtUtils {
     }
 
     /**
-     * 解析要验证的签名(因为签名的几部分是用base64编码的，先解析出来)
-     *
-     * @param toVerifySign
-     *                     要验证的签名
-     *
-     * @return 返回SignedJWT的实体(可从中取出head和payload部分进行验证，再校验签名是否正确)
-     *
-     * @throws ParseException
-     *                        解析失败
-     */
-    public static SignedJWT parse(final String toVerifySign) throws ParseException {
-        log.info("解析JWT签名: {}", toVerifySign);
-        if (StringUtils.isBlank(toVerifySign)) {
-            throw new IllegalArgumentException("JWT签名不能为空");
-        }
-        return SignedJWT.parse(toVerifySign);
-    }
-
-    /**
-     * 校验签名是否正确
-     *
-     * @param key
-     *                  签名的密钥
-     * @param signedJWT
-     *                  签名的实体
-     *
-     * @return 校验是否正确
-     *
-     * @throws JOSEException
-     *                       校验失败
-     */
-    public static boolean verify(final byte[] key, final SignedJWT signedJWT) throws JOSEException {
-        log.info("校验签名是否正确");
-        final JWSVerifier verifier = new MACVerifier(key);
-        final boolean     result   = signedJWT.verify(verifier);
-        if (result) {
-            log.info("JWT的签名正确");
-        }
-        else {
-            log.info("JWT的签名不正确");
-        }
-        return result;
-    }
-
-    /**
-     * 从签名中获取JWT项的集合
-     */
-    public static JWTClaimsSet getJwtItemsFromSign(final String sign) throws ParseException {
-        // 解析签名
-        final SignedJWT signedJWT = JwtUtils.parse(sign);
-        // 从签名中获取JWT项的集合
-        return signedJWT.getJWTClaimsSet();
-    }
-
-    /**
-     * 从签名中获取JWT的指定项
-     */
-    public static Object getJwtItemFromSign(final String sign, final String key) throws ParseException {
-        return getJwtItemsFromSign(sign).getClaim(key);
-    }
-
-    /**
      * 从签名中获取JWT信息中的账户ID
      *
      * @return 如果没有此项，会抛出NumberFormatException异常
      */
-    public static Long getJwtAccountIdFromSign(final String sign) {
+    public static Long getJwtAccountIdFromSign(String sign)
+    {
         try {
-            return Long.valueOf((String) getJwtItemFromSign(sign, "accountId"));
+            SignedJWT jwt = SignedJWT.parse(sign);
+            return Long.valueOf(jwt.getJWTClaimsSet().getSubject());
         } catch (final ParseException e) {
             return null;
         }
@@ -220,24 +72,26 @@ public class JwtUtils {
      * 从签名中获取JWT信息中的附加信息
      *
      * @return 返回Map&lt;String, Object&gt;，再通过key可获得里面的项
-     *         例如:
-     *         result.get("orgId")可获得当前用户的组织ID
-     *         result.get("isTester")可获得当前用户是否是测试者
+     * 例如:
+     * result.get("orgId")可获得当前用户的组织ID
+     * result.get("isTester")可获得当前用户是否是测试者
      */
-    @SuppressWarnings("unchecked")
-    public static Map<String, Object> getJwtAdditionFromSign(final String sign) throws ParseException {
-        return (Map<String, Object>) getJwtItemFromSign(sign, "addition");
+    public static Map<String, Object> getJwtAdditionFromSign(final String sign) throws ParseException
+    {
+        SignedJWT jwt = SignedJWT.parse(sign);
+        return (Map<String, Object>) jwt.getJWTClaimsSet().getClaim("addition");
     }
 
     /**
      * 从签名中获取JWT信息中的附加信息中的项
      *
      * @return 返回通过key可获得里面的项
-     *         例如:
-     *         "orgId"可获得当前用户的组织ID
-     *         "isTester"可获得当前用户是否是测试者
+     * 例如:
+     * "orgId"可获得当前用户的组织ID
+     * "isTester"可获得当前用户是否是测试者
      */
-    public static Object getJwtAdditionItemFromSign(final String sign, final String key) throws ParseException {
+    public static Object getJwtAdditionItemFromSign(final String sign, final String key) throws ParseException
+    {
         final Map<String, Object> additions = getJwtAdditionFromSign(sign);
         return additions == null ? null : additions.get(key);
     }
