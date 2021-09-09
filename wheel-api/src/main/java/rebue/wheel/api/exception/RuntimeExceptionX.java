d277cb6061d3b845d4e5c948a467400bffd93be6
zbz <nnzbz@21cn.com>
2021-09-09 14:45:27 +0800

    * RuntimeExceptionX加上errorCode字段

--- wheel-api/src/main/java/rebue/wheel/api/exception/RuntimeExceptionX.java
+++ wheel-api/src/main/java/rebue/wheel/api/exception/RuntimeExceptionX.java
@@ -1,23 +1,37 @@
 package rebue.wheel.api.exception;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * 扩展RuntimeException，详细记录异常日志
  */
 @Slf4j
+@Data
 public class RuntimeExceptionX extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
+    private String            errorCode;
+
     public RuntimeExceptionX(final String msg) {
-        super(msg);
-        log.error(msg);
+        this(null, msg);
     }
 
     public RuntimeExceptionX(final String msg, final Throwable t) {
+        this(null, msg, t);
+    }
+
+    public RuntimeExceptionX(String errorCode, final String msg) {
+        super(msg);
+        this.errorCode = errorCode;
+        log.error(errorCode == null ? msg : errorCode + msg);
+    }
+
+    public RuntimeExceptionX(String errorCode, final String msg, final Throwable t) {
         super(msg, t);
-        log.error(msg, t);
+        this.errorCode = errorCode;
+        log.error(errorCode == null ? msg : errorCode + msg, t);
     }
 
 }
