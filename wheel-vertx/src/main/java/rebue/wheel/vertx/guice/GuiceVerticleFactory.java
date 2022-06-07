b277c9599308868c7bce517cb9c6dfe02a5d0002
zbz <nnzbz@21cn.com>
2022-06-07 10:53:56 +0800

    * 统一共用注入器

--- wheel-vertx/src/main/java/rebue/wheel/vertx/guice/GuiceVerticleFactory.java
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/guice/GuiceVerticleFactory.java
@@ -1,43 +1,28 @@
 
 package rebue.wheel.vertx.guice;
 
-import java.util.List;
-import java.util.ServiceLoader;
 import java.util.concurrent.Callable;
 
-import com.google.common.collect.Lists;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Module;
 
 import io.vertx.core.Promise;
 import io.vertx.core.Verticle;
-import io.vertx.core.Vertx;
 import io.vertx.core.impl.verticle.CompilingClassLoader;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.spi.VerticleFactory;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class GuiceVerticleFactory implements VerticleFactory {
 
-    private JsonObject config;
-    private Injector   injector;
+    private Injector injector;
 
-    public GuiceVerticleFactory(final JsonObject config) {
-        this.config = config;
+    public GuiceVerticleFactory(final Injector injector) {
+        this.injector = injector;
     }
 
     @Override
     public String prefix() {
         return "guice";
-    }
-
-    @Override
-    public void init(final Vertx vertx) {
-        final List<Module> modules = Lists.<Module>newLinkedList(ServiceLoader.load(Module.class));
-        modules.add(new VertxGuiceModule(vertx, this.config));
-        this.injector = Guice.createInjector(modules);
     }
 
     @Override
--- wheel-vertx/src/main/java/rebue/wheel/vertx/verticle/AbstractMainVerticle.java
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/verticle/AbstractMainVerticle.java
@@ -1,6 +1,5 @@
 package rebue.wheel.vertx.verticle;
 
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -10,27 +9,27 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
 
 import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.CompositeFuture;
-import io.vertx.core.Context;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Verticle;
-import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.jackson.DatabindCodec;
 import lombok.extern.slf4j.Slf4j;
 import rebue.wheel.vertx.guice.GuiceVerticleFactory;
+import rebue.wheel.vertx.guice.VertxGuiceModule;
 
 @SuppressWarnings("deprecation")
 @Slf4j
 public abstract class AbstractMainVerticle extends AbstractVerticle {
-    public static final String                       EVENT_BUS_DEPLOY_SUCCESS = "rebue.wheel.vertx.verticle.main-verticle.deploy-success";
-
-    protected Map<String, Class<? extends Verticle>> verticleClasses          = new LinkedHashMap<>();
+    public static final String EVENT_BUS_DEPLOY_SUCCESS = "rebue.wheel.vertx.verticle.main-verticle.deploy-success";
 
     static {
         // 初始化jackson的功能
@@ -43,14 +42,6 @@ public abstract class AbstractMainVerticle extends AbstractVerticle {
                 )
                 .setSerializationInclusion(Include.NON_NULL)                // 不序列化值为null的字段
                 .registerModule(new JavaTimeModule());                      // 支持Java8的LocalDate/LocalDateTime类型
-    }
-
-    protected abstract void initVerticleClasses();
-
-    @Override
-    public void init(final Vertx vertx, final Context context) {
-        super.init(vertx, context);
-        initVerticleClasses();
     }
 
     @Override
@@ -70,13 +61,20 @@ public abstract class AbstractMainVerticle extends AbstractVerticle {
                 return;
             }
 
+            log.info("创建注入器");
+            final List<Module> guiceModules = new LinkedList<>();
+            guiceModules.add(new VertxGuiceModule(this.vertx, config));
+            addGuiceModules(guiceModules);
+            final Injector injector = Guice.createInjector(guiceModules);
+
             log.info("注册GuiceVerticleFactory工厂");
-            this.vertx.registerVerticleFactory(new GuiceVerticleFactory(config));
+            this.vertx.registerVerticleFactory(new GuiceVerticleFactory(injector));
 
             log.info("部署verticle");
+            final Map<String, Class<? extends Verticle>> verticleClasses = getVerticleClasses();
             @SuppressWarnings("rawtypes")
-            final List<Future> deployFutures = new LinkedList<>();
-            for (final Entry<String, Class<? extends Verticle>> entry : this.verticleClasses.entrySet()) {
+            final List<Future>                           deployFutures   = new LinkedList<>();
+            for (final Entry<String, Class<? extends Verticle>> entry : verticleClasses.entrySet()) {
                 deployFutures.add(this.vertx.deployVerticle("guice:" + entry.getValue().getName(), new DeploymentOptions(config.getJsonObject(entry.getKey()))));
             }
 
@@ -95,5 +93,17 @@ public abstract class AbstractMainVerticle extends AbstractVerticle {
                     });
         });
     }
+
+    /**
+     * 添加guice模块
+     *
+     * @param guiceModules 添加guice模块到此列表
+     */
+    protected abstract void addGuiceModules(List<Module> guiceModules);
+
+    /**
+     * 初始化要部署的Verticle类列表
+     */
+    protected abstract Map<String, Class<? extends Verticle>> getVerticleClasses();
 
 }
