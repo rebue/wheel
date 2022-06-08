32053d1469973963c06fc2f26a2e7f195e8a4457
zbz <nnzbz@21cn.com>
2022-06-08 22:17:25 +0800

    - 删除WebGuiceModule

--- wheel-vertx/src/main/java/rebue/wheel/vertx/guice/OracleGuiceModule.java
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/guice/OracleGuiceModule.java
@@ -3,6 +3,8 @@ package rebue.wheel.vertx.guice;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import org.apache.logging.log4j.jul.Log4jBridgeHandler;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 
@@ -12,6 +14,11 @@ import io.vertx.sqlclient.Pool;
 import rebue.wheel.vertx.util.OracleUtils;
 
 public class OracleGuiceModule extends AbstractModule {
+
+    static {
+        // log4j支持jul
+        Log4jBridgeHandler.install(true, "log4j", true);
+    }
 
     @Singleton
     @Provides
