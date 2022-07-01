514dc714287f9f71eed73c063934b0c735bbd6a7
zbz <nnzbz@21cn.com>
2022-07-01 18:02:34 +0800

    * Redis支持集群模式

--- wheel-vertx/src/main/java/rebue/wheel/vertx/util/RedisUtils.java
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/util/RedisUtils.java
@@ -1,11 +1,16 @@
 package rebue.wheel.vertx.util;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.redis.client.Redis;
 import io.vertx.redis.client.RedisAPI;
+import io.vertx.redis.client.RedisClientType;
 import io.vertx.redis.client.RedisOptions;
+import io.vertx.redis.client.RedisReplicas;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class RedisUtils {
 
     /**
@@ -13,7 +18,16 @@ public class RedisUtils {
      * an exception in the connection.
      */
     public static RedisAPI createRedisClient(final Vertx vertx, final JsonObject redisConfig) {
-        final RedisOptions redisOptions = new RedisOptions(redisConfig);
+        final RedisOptions redisOptions      = new RedisOptions(redisConfig);
+        final JsonArray    connectionStrings = redisConfig.getJsonArray("connectionStrings");
+        if (connectionStrings != null) {
+            log.info("Redis连接设置为集群模式: {}", connectionStrings);
+            redisOptions.setType(RedisClientType.CLUSTER);
+            redisOptions.setUseReplicas(RedisReplicas.SHARE);
+            for (final Object connectionString : connectionStrings) {
+                redisOptions.addConnectionString((String) connectionString);
+            }
+        }
         return RedisAPI.api(Redis.createClient(vertx, redisOptions));
     }
 
