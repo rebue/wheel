fd9c66a1868cc3938af82cc51021c3ca8a5bf56a
zbz <nnzbz@21cn.com>
2022-08-06 11:29:47 +0800

    * 补充说明

--- wheel-vertx/src/main/java/rebue/wheel/vertx/guice/OracleGuiceModule.java
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/guice/OracleGuiceModule.java
@@ -17,6 +17,7 @@ public class OracleGuiceModule extends AbstractModule {
 
     static {
         // log4j支持jul
+        // FIXME 下面这句会导致vertx的服务启动时报“Cannot bind Unsafe.defineAnonymousClass---com.google.inject.internal.aop.UnsafeClassDefiner.log4j”，但是debug级别的，不知道会有什么问题
         Log4jBridgeHandler.install(true, "log4j", true);
     }
 
