9471e1aefece1cd3364cdf8e87b4550f115f9e24
nnzbz <nnzbz@qq.com>
2022-08-24 10:27:13 +0800

    + oracle支持配置properties

--- wheel-vertx/src/main/java/rebue/wheel/vertx/util/OracleUtils.java
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/util/OracleUtils.java
@@ -1,5 +1,7 @@
 package rebue.wheel.vertx.util;
 
+import java.util.Map.Entry;
+
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.oracleclient.OracleConnectOptions;
@@ -12,8 +14,15 @@ public class OracleUtils {
      * 创建客户端
      */
     public static OraclePool createClient(final Vertx vertx, final JsonObject oracleConfig) {
-        final OracleConnectOptions connectOptions = oracleConfig.getJsonObject(OracleConfig.CONNECT_PREFIX).mapTo(OracleConnectOptions.class);
-        final PoolOptions          poolOptions    = oracleConfig.getJsonObject(OracleConfig.POOL_PREFIX).mapTo(PoolOptions.class);
+        final JsonObject           config         = oracleConfig.getJsonObject(OracleConfig.CONNECT_PREFIX);
+        final OracleConnectOptions connectOptions = config.mapTo(OracleConnectOptions.class);
+        final JsonObject           properties     = config.getJsonObject("properties");
+        if (!properties.isEmpty()) {
+            for (final Entry<String, Object> entry : properties) {
+                connectOptions.addProperty(entry.getKey(), entry.getValue().toString());
+            }
+        }
+        final PoolOptions poolOptions = oracleConfig.getJsonObject(OracleConfig.POOL_PREFIX).mapTo(PoolOptions.class);
         return OraclePool.pool(vertx, connectOptions, poolOptions.setShared(true));
     }
 }
