36c3dbd6b651733c71c81a0f172bddddef5771a8
nnzbz <nnzbz@qq.com>
2022-10-25 22:39:35 +0800

    * 补充注释

--- wheel-vertx/src/main/java/rebue/wheel/vertx/config/XxlJobProperties.java
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/config/XxlJobProperties.java
@@ -22,6 +22,7 @@ public class XxlJobProperties {
 
         /**
          * 调度中心部署根地址 [选填]：如调度中心集群部署存在多个地址则用逗号分隔。执行器将会使用该地址进行"执行器心跳注册"和"任务结果回调"；为空则关闭自动注册；
+         * XXX 注意这里似乎只能用IP地址，否则start的时候，服务器会返回400
          */
         private String addresses;
 
