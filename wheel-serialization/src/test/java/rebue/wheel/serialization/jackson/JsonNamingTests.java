33f94067f8baeb4587d2b039a191659a46c2c8ac
nnzbz <nnzbz@qq.com>
2022-11-03 21:24:36 +0800

    + jackson添加灵活的命名策略

---
+++ wheel-serialization/src/test/java/rebue/wheel/serialization/jackson/JsonNamingTests.java
@@ -0,0 +1,21 @@
+package rebue.wheel.serialization.jackson;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JsonNamingTests {
+    @Test
+    public void test01() throws IOException {
+        Student student = new Student(1L, "N001", "张三", (short) 28, new Date());
+        log.info("student serialize: {}", JacksonUtils.serialize(student));
+        student = JacksonUtils.deserialize("{\"id\":1,\"num\":\"N001\",\"name\":\"张三\",\"age\":28,\"birthDate\":\"2022-11-03T11:33:47.037+08:00\"}", Student.class);
+        log.info("student deserialize: {}", student);
+        student = JacksonUtils.deserialize("{\"id\":1,\"num\":\"N001\",\"name\":\"张三\",\"age\":28,\"birth_date\":\"2022-11-03T11:33:47.037+08:00\"}", Student.class);
+        log.info("student deserialize: {}", student);
+    }
+}
--- wheel-serialization/src/test/java/rebue/wheel/serialization/jackson/Student.java
+++ wheel-serialization/src/test/java/rebue/wheel/serialization/jackson/Student.java
@@ -1,20 +1,28 @@
-package rebue.wheel.serialization.protostuff;
+package rebue.wheel.serialization.jackson;
 
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@JsonNaming(PropertyNamingStrategiesEx.SnakeCaseStrategy.class)
 public class Student implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long              id;
-    private String            code;
+    private String            num;
     private String            name;
     private short             age;
-    private Date              birthDay;
+    private Date              birthDate;
+
 }
