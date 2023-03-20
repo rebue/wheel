3d2691790479e4917bf775673efa1872ef8d39b9
zbz <nnzbz@21cn.com>
2023-03-20 18:47:25 +0800

    + WebClient添加RequestOptions可配置

--- wheel-vertx/src/main/java/rebue/wheel/vertx/guice/WebClientGuiceModule.java
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/guice/WebClientGuiceModule.java
@@ -1,16 +1,16 @@
 package rebue.wheel.vertx.guice;
 
-import javax.inject.Named;
-import javax.inject.Singleton;
-
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-
 import io.vertx.core.Vertx;
+import io.vertx.core.http.RequestOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
 
 @Slf4j
 public class WebClientGuiceModule extends AbstractModule {
@@ -28,6 +28,14 @@ public class WebClientGuiceModule extends AbstractModule {
             return WebClient.create(vertx);
         }
         return WebClient.create(vertx, new WebClientOptions(webClientConfig));
+    }
+
+    @Singleton
+    @Provides
+    RequestOptions getRequestOptions(@Named("config") final JsonObject config) {
+        log.info("WebClientGuiceModule.getRequestOptions");
+        final JsonObject requestOptionsConfig = config.getJsonObject("requestOptions");
+        return (requestOptionsConfig == null) ? new RequestOptions() : new RequestOptions(requestOptionsConfig);
     }
 
 }
