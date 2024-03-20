55b1bffe11582448f893947365d3dbdf440448c7
zbz <nnzbz@21cn.com>
2024-03-20 21:27:59 +0800

    * 更改Ra的返回结构扁平化

--- wheel-api/src/main/java/rebue/wheel/api/ra/PageRa.java
+++ wheel-api/src/main/java/rebue/wheel/api/ra/PageRa.java
@@ -2,13 +2,13 @@ package rebue.wheel.api.ra;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.github.pagehelper.PageInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.lang.NonNull;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * 带有分页信息的附加内容
@@ -16,15 +16,23 @@ import java.io.Serializable;
  */
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor // 不知道@Data中默认包含的@RequiredArgsConstructor为何没起效
+@AllArgsConstructor
+@Builder
 @JsonInclude(Include.NON_NULL)
 public class PageRa<T> implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
-     * 返回分页信息
+     * 当前页
      */
-    @NonNull
-    private PageInfo<T> page;
+    private   Integer pageNum;
+    /**
+     * 总记录数
+     */
+    protected Long    total;
+    /**
+     * 结果集
+     */
+    private   List<T> list;
 
 }
