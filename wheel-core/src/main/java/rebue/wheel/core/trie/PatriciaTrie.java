ff39de7530ee90daffad619c4f5096068b17fe55
zbz <nnzbz@21cn.com>
2024-05-30 18:37:59 +0800

    * 修正trie的算法bug

--- wheel-core/src/main/java/rebue/wheel/core/trie/PatriciaTrie.java
+++ wheel-core/src/main/java/rebue/wheel/core/trie/PatriciaTrie.java
@@ -4,12 +4,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PatriciaTrie<V> implements Trie<V> {
-    private TrieNode<V> root = new TrieNode<>();
+    private final TrieNode<V> root = new TrieNode<>();
 
     /**
      * 叶子数量
      */
-    private int         size = 0;
+    private int               size = 0;
 
     @Override
     public int size() {
@@ -18,15 +18,16 @@ public class PatriciaTrie<V> implements Trie<V> {
 
     @Override
     public void put(String key, V value) {
-        if (key == null || key.length() == 0 || key.isEmpty()) {
+        if (key == null || key.length() == 0) {
             return;
         }
+        size++;
         put(root, key.trim(), value);
     }
 
     @Override
     public V get(String key) {
-        if (key == null || key.length() == 0 || key.isEmpty()) {
+        if (key == null || key.length() == 0) {
             return null;
         }
         return get(root, key);
@@ -34,7 +35,7 @@ public class PatriciaTrie<V> implements Trie<V> {
 
     @Override
     public List<V> list(String key) {
-        if (key == null || key.length() == 0 || key.isEmpty()) {
+        if (key == null || key.length() == 0) {
             return null;
         }
         List<V> list = new LinkedList<>();
@@ -50,35 +51,30 @@ public class PatriciaTrie<V> implements Trie<V> {
         LOOP_CHILDREN: for (int i = 0; i < node.getChildren().size(); i++) {
             TrieNode<V> child     = node.getChildren().get(i);
             String      childKey  = child.getKey();
-            int         minLength = childKey.length() < key.length() ? childKey.length() : key.length();
+            int         minLength = Math.min(childKey.length(), key.length());
             // 循环遍历子节点的key的字符
             for (int j = 0; j < minLength; j++) {
                 // 比较字符
                 int compared = key.charAt(j) - childKey.charAt(j);
-                // 如果是一样的字符，继续查找，直到找到不同的字符，如果遍历完所有字符也一直相等，那么在循环外再进行逻辑处理
-                if (compared == 0) {
-                    continue;
-                }
+
                 // 如果key的当前字符小于childKey的当前字符
-                else if (compared < 0) {
+                if (compared < 0) {
                     // 如果第一个字符就小了，直接在最前面插入一个新的子节点
                     if (j == 0) {
-                        size++;
                         node.getChildren().add(j, TrieNode.of(true, key, value));
                         return;
                     }
                     // 如果之前有相同的字符，那么在此处插入一个新的叶子节点，并将当前子节点移动到新的叶子节点下
                     node.getChildren().remove(i);
                     TrieNode<V> newNode = TrieNode.of(childKey.substring(0, j));
-                    size++;
                     newNode.getChildren().add(TrieNode.of(true, key.substring(j), value));
-                    child.setKey(childKey.substring(j));
                     newNode.getChildren().add(child);
-                    node.getChildren().add(j, newNode);
+                    child.setKey(childKey.substring(j));
+                    node.getChildren().add(i, newNode);
                     return;
                 }
                 // 如果key的当前字符大于childKey的当前字符
-                else {
+                else if (compared > 0) {
                     // 如果第一个字符就大了，继续查找下一个子节点，直到找到有相同的字符进行插入，如果直到遍历完所有子节点一直第一个字符就大了，那么在循环外再进行逻辑处理
                     if (j == 0) {
                         continue LOOP_CHILDREN;
@@ -86,13 +82,13 @@ public class PatriciaTrie<V> implements Trie<V> {
                     // 如果之前有相同的字符，那么在此处插入一个新的叶子节点，并将当前子节点移动到新的叶子节点下
                     node.getChildren().remove(i);
                     TrieNode<V> newNode = TrieNode.of(childKey.substring(0, j));
-                    size++;
-                    newNode.getChildren().add(TrieNode.of(true, key.substring(j), value));
                     child.setKey(childKey.substring(j));
                     newNode.getChildren().add(child);
-                    node.getChildren().add(j, newNode);
+                    newNode.getChildren().add(TrieNode.of(true, key.substring(j), value));
+                    node.getChildren().add(i, newNode);
                     return;
                 }
+                // 如果是一样的字符，继续查找，直到找到不同的字符，如果遍历完所有字符也一直相等，那么在循环外再进行逻辑处理
             }
             // 如果遍历完所有字符，说明key与childKey前缀相同，做如下判断:
             // 1. key的长度等于childKey的长度，说明key和childKey是完全一样的，将此子节点设置为叶子节点
@@ -100,11 +96,13 @@ public class PatriciaTrie<V> implements Trie<V> {
             // 3. key的长度大于childKey的长度，则分割key，递归调用本方法，继续在当前子节点中添加key
             int compared = key.length() - childKey.length();
             if (compared == 0) {
-                child.setIsLeaf(true);
+                if (!child.getIsLeaf()) {
+                    size--;
+                    child.setIsLeaf(true);
+                }
                 child.setValue(value);
                 return;
             } else if (compared < 0) {
-                size++;
                 TrieNode<V> newNode = TrieNode.of(true, key, value);
                 node.getChildren().remove(i);
                 child.setKey(childKey.substring(minLength));
@@ -126,21 +124,17 @@ public class PatriciaTrie<V> implements Trie<V> {
         LOOP_CHILDREN: for (int i = 0; i < node.getChildren().size(); i++) {
             TrieNode<V> child     = node.getChildren().get(i);
             String      childKey  = child.getKey();
-            int         minLength = childKey.length() < key.length() ? childKey.length() : key.length();
+            int         minLength = Math.min(childKey.length(), key.length());
             // 循环遍历子节点的key的字符
             for (int j = 0; j < minLength; j++) {
                 // 比较字符
                 int compared = key.charAt(j) - childKey.charAt(j);
-                // 如果是一样的字符，继续查找，直到找到不同的字符，如果遍历完所有字符也一直相等，那么在循环外再进行逻辑处理
-                if (compared == 0) {
-                    continue;
-                }
                 // 如果key的当前字符小于childKey的当前字符，则说明没有此key，返回null
-                else if (compared < 0) {
+                if (compared < 0) {
                     return null;
                 }
                 // 如果key的当前字符大于childKey的当前字符
-                else {
+                else if (compared > 0) {
                     // 如果第一个字符就大了，继续查找下一个子节点，如果直到遍历完所有子节点一直第一个字符就大了，那么在循环外再进行逻辑处理
                     if (j == 0) {
                         continue LOOP_CHILDREN;
@@ -148,6 +142,7 @@ public class PatriciaTrie<V> implements Trie<V> {
                     // 如果之前有相同的字符，那么说明没有此key，返回null
                     return null;
                 }
+                // 如果是一样的字符，继续查找，直到找到不同的字符，如果遍历完所有字符也一直相等，那么在循环外再进行逻辑处理
             }
             // 如果遍历完所有字符，说明key与childKey前缀相同，做如下判断:
             // 1. key的长度等于childKey的长度，说明key和childKey是完全一样的，返回此节点
@@ -155,7 +150,7 @@ public class PatriciaTrie<V> implements Trie<V> {
             // 3. key的长度大于childKey的长度，如果当前子节点已经是叶子节点了，返回此节点，否则还要继续在下一层中查找，递归调用本方法
             int compared = key.length() - childKey.length();
             if (compared == 0) {
-                return child.getValue();
+                return child.getIsLeaf() ? child.getValue() : null;
             } else if (compared < 0) {
                 return null;
             } else {
@@ -171,21 +166,17 @@ public class PatriciaTrie<V> implements Trie<V> {
         LOOP_CHILDREN: for (int i = 0; i < node.getChildren().size(); i++) {
             TrieNode<V> child     = node.getChildren().get(i);
             String      childKey  = child.getKey();
-            int         minLength = childKey.length() < key.length() ? childKey.length() : key.length();
+            int         minLength = Math.min(childKey.length(), key.length());
             // 循环遍历子节点的key的字符
             for (int j = 0; j < minLength; j++) {
                 // 比较字符
                 int compared = key.charAt(j) - childKey.charAt(j);
-                // 如果是一样的字符，继续查找，直到找到不同的字符，如果遍历完所有字符也一直相等，那么在循环外再进行逻辑处理
-                if (compared == 0) {
-                    continue;
-                }
                 // 如果key的当前字符小于childKey的当前字符，则说明没有此key，返回
-                else if (compared < 0) {
+                if (compared < 0) {
                     return;
                 }
                 // 如果key的当前字符大于childKey的当前字符
-                else {
+                else if (compared > 0) {
                     // 如果第一个字符就大了，继续查找下一个子节点，如果直到遍历完所有子节点一直第一个字符就大了，那么在循环外再进行逻辑处理
                     if (j == 0) {
                         continue LOOP_CHILDREN;
@@ -193,6 +184,7 @@ public class PatriciaTrie<V> implements Trie<V> {
                     // 如果之前有相同的字符，那么说明没有此key，返回
                     return;
                 }
+                // 如果是一样的字符，继续查找，直到找到不同的字符，如果遍历完所有字符也一直相等，那么在循环外再进行逻辑处理
             }
             // 如果遍历完所有字符，说明key与childKey前缀相同，做如下判断:
             // 1. key的长度等于childKey的长度，说明key和childKey是完全一样的，添加此节点并返回
@@ -200,7 +192,9 @@ public class PatriciaTrie<V> implements Trie<V> {
             // 3. key的长度大于childKey的长度，如果当前子节点已经是叶子节点了，添加此节点，然后还要继续在下一层中查找，递归调用本方法
             int compared = key.length() - childKey.length();
             if (compared == 0) {
-                list.add(child.getValue());
+                if (child.getIsLeaf()) {
+                    list.add(child.getValue());
+                }
                 return;
             } else if (compared < 0) {
                 return;
--- wheel-core/src/test/java/rebue/wheel/core/trie/PatriciaTrieTests.java
+++ wheel-core/src/test/java/rebue/wheel/core/trie/PatriciaTrieTests.java
@@ -14,19 +14,32 @@ public class PatriciaTrieTests {
         trie.put("aaa", "AAA");
         trie.put("ab", "AB");
         trie.put("abc", "ABC");
+        trie.put("abcabc", "ABCABC");
+        trie.put("abcdef", "ABCDEF");
         trie.put("b", "B");
         trie.put("c", "C");
-        trie.put("d", "d");
+        trie.put("d", "D");
+        trie.put("GET:/abc", "GET ABC");
+        trie.put("GET:/def", "GET DEF");
 
         // Retrieve values from the trie
-        System.out.println(trie.get("/"));              // Output: null
-        System.out.println(trie.get("a"));              // Output: A
-        System.out.println(trie.get("b"));              // Output: B
-        System.out.println(trie.get("c"));              // Output: C
-        System.out.println(trie.get("ab"));             // Output: A
-        System.out.println(trie.list("ab"));            // Output: A,AB
-        System.out.println(trie.list("abc"));           // Output: A,AB,ABC
-        System.out.println(trie.list("abcdef"));        // Output: A,AB,ABC
+        System.out.println(trie.get("/"));                      // Output: null
+        System.out.println(trie.get("a"));                      // Output: A
+        System.out.println(trie.get("b"));                      // Output: B
+        System.out.println(trie.get("c"));                      // Output: C
+        System.out.println(trie.get("ab"));                     // Output: A
+        System.out.println(trie.list("ab"));               // Output: A,AB
+        System.out.println(trie.list("abc"));              // Output: A,AB,ABC
+        System.out.println(trie.list("abcabc"));           // Output: A,AB,ABC,ABCABC
+        System.out.println(trie.list("abcdef"));           // Output: A,AB,ABC,ABCDEF
+        System.out.println(trie.list("b"));                 // Output: B
+        System.out.println(trie.list("c"));                 // Output: C
+        System.out.println(trie.list("d"));                 // Output: D
+        System.out.println(trie.get("GET:/"));                  // Output: null
+        System.out.println(trie.get("GET:/abc"));                  // Output: GET ABC
+        System.out.println(trie.get("GET:/abc/def"));                  // Output: GET ABC
+        System.out.println(trie.list("GET:/"));             // Output: null
+        System.out.println(trie.list("GET:/def"));             // Output: [GET DEF]
     }
 
 }
