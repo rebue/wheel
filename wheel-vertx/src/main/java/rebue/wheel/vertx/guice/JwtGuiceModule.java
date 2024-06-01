89207a5b359e5582bdbdbe0d92d37bb9abc3392f
zbz <nnzbz@21cn.com>
2024-06-01 11:31:25 +0800

    * 初步完成网关身份认证

---
+++ wheel-vertx/src/main/java/rebue/wheel/vertx/guice/JwtGuiceModule.java
@@ -0,0 +1,29 @@
+package rebue.wheel.vertx.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTAuthOptions;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JwtGuiceModule extends AbstractModule {
+
+    public JwtGuiceModule() {
+        log.info("new JwtGuiceModule");
+    }
+
+    @Singleton
+    @Provides
+    JWTAuth getJwtAuth(Vertx vertx, @Named("config") final JsonObject config) {
+        log.info("JwtGuiceModule.getJwtAuth");
+        JWTAuthOptions options = new JWTAuthOptions(config.getJsonObject("jwt"));
+        return JWTAuth.create(vertx, options);
+    }
+
+}
