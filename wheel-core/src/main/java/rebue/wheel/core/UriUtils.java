310901843b864d5b227810f7c043e3c884b6bfd4
zbz <nnzbz@qq.com>
2024-11-08 09:23:04 +0800

    * 添加查询参数的方法兼容包含#的情况

--- wheel-core/src/main/java/rebue/wheel/core/UriUtils.java
+++ wheel-core/src/main/java/rebue/wheel/core/UriUtils.java
@@ -2,8 +2,10 @@ package rebue.wheel.core;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class UriUtils {
     /**
@@ -37,6 +39,14 @@ public class UriUtils {
      */
     public static String addQueryParam(String uri, String queryParamName, String queryParamValue) {
         String queryParamPath = queryParamName + "=" + URLEncoder.encode(queryParamValue, StandardCharsets.UTF_8);
+
+        String hash = null;
+        if (uri.contains("#")) {
+            String[] uriSplit = uri.split("#");
+            uri  = uriSplit[0];
+            hash = uriSplit[1];
+        }
+
         // 已有参数列表
         if (uri.contains("?")) {
             // 已有该参数
@@ -56,17 +66,93 @@ public class UriUtils {
                         list.add(queryParam);
                     }
                 }
-                return baseUri + "?" + String.join("&", list);
+                uri = baseUri + "?" + String.join("&", list);
             }
             // 没有该参数
             else {
-                queryParamPath = "&" + queryParamPath;
+                uri += "&" + queryParamPath;
             }
         }
         // 没有参数列表
         else {
-            queryParamPath = "?" + queryParamPath;
+            uri += "?" + queryParamPath;
         }
-        return uri + queryParamPath;
+
+        if (hash != null) {
+            uri += "#" + hash;
+        }
+        return uri;
     }
+
+    /**
+     * 将查询参数字符串转成Map对象
+     * 
+     * @param queryParams 查询参数
+     * @return Map对象
+     */
+    public static Map<String, String> queryParamsToMap(String queryParams) {
+        String[]            queryParamsSplit = queryParams.split("&");
+        Map<String, String> map              = new HashMap<>();
+        for (String queryParam : queryParamsSplit) {
+            String[] queryParamSplit = queryParam.split("=");
+            String   paramName       = queryParamSplit[0];
+            String   paramValue      = queryParamSplit[1];
+            map.put(paramName, paramValue);
+        }
+        return map;
+    }
+
+    /**
+     * 将Map对象转成查询参数字符串
+     *
+     * @param map Map对象
+     * @return 查询参数字符串
+     */
+    public static String mapToQueryParams(Map<String, String> map) {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (!sb.isEmpty()) {
+                sb.append("&");
+            }
+            sb.append(entry.getKey()).append("=").append(entry.getValue());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 添加多个查询参数
+     *
+     * @param uri         要替换查询参数的uri
+     * @param queryParams 多个查询参数
+     * @return 添加后的URI
+     */
+    public static String addQueryParams(String uri, String queryParams) {
+        String hash = null;
+        if (uri.contains("#")) {
+            String[] uriSplit = uri.split("#");
+            uri  = uriSplit[0];
+            hash = uriSplit[1];
+        }
+
+        // 已有参数列表
+        if (uri.contains("?")) {
+            String[]            uriSplit             = uri.split("\\?");
+            String              baseUri              = uriSplit[0];
+            String              originQueryParams    = uriSplit[1];
+            Map<String, String> queryParamsMap       = queryParamsToMap(queryParams);
+            Map<String, String> originQueryParamsMap = queryParamsToMap(originQueryParams);
+            originQueryParamsMap.putAll(queryParamsMap);
+            uri = baseUri + "?" + mapToQueryParams(originQueryParamsMap);
+        }
+        // 没有参数列表
+        else {
+            uri += "?" + queryParams;
+        }
+
+        if (hash != null) {
+            uri += "#" + hash;
+        }
+        return uri;
+    }
+
 }
--- wheel-core/src/test/java/rebue/wheel/core/UriUtilsTests.java
+++ wheel-core/src/test/java/rebue/wheel/core/UriUtilsTests.java
@@ -6,13 +6,31 @@ import org.junit.jupiter.api.Test;
 public class UriUtilsTests {
 
     @Test
-    void addQueryParam() {
+    void testRemoveEndSlash() {
+        Assertions.assertEquals("https://www.abc.com", UriUtils.removeEndSlash("https://www.abc.com"));
+        Assertions.assertEquals("https://www.abc.com", UriUtils.removeEndSlash("https://www.abc.com/"));
+    }
+
+    @Test
+    void testAddQueryParam() {
         Assertions.assertEquals("https://www.abc.com?a=%E5%BC%A0%E4%B8%89",
                 UriUtils.addQueryParam("https://www.abc.com", "a", "张三"));
         Assertions.assertEquals("https://www.abc.com?a=%E5%BC%A0%E4%B8%89",
                 UriUtils.addQueryParam("https://www.abc.com?a=%E6%9D%8E%E5%9B%9B", "a", "张三"));
         Assertions.assertEquals("https://www.abc.com?a=%E5%BC%A0%E4%B8%89&b=%E7%8E%8B%E4%BA%94",
                 UriUtils.addQueryParam("https://www.abc.com?a=%E6%9D%8E%E5%9B%9B&b=%E7%8E%8B%E4%BA%94", "a", "张三"));
+    }
+
+    @Test
+    void testAddQueryParams() {
+        Assertions.assertEquals("https://www.abc.com?a=%E5%BC%A0%E4%B8%89",
+                UriUtils.addQueryParams("https://www.abc.com", "a=%E5%BC%A0%E4%B8%89"));
+        Assertions.assertEquals("https://www.abc.com?a=%E5%BC%A0%E4%B8%89&b=%E6%9D%8E%E5%9B%9B",
+                UriUtils.addQueryParams("https://www.abc.com", "a=%E5%BC%A0%E4%B8%89&b=%E6%9D%8E%E5%9B%9B"));
+        Assertions.assertEquals("https://www.abc.com?a=%E5%BC%A0%E4%B8%89&b=%E6%9D%8E%E5%9B%9B",
+                UriUtils.addQueryParams("https://www.abc.com?a=%E6%9D%8E%E5%9B%9B", "a=%E5%BC%A0%E4%B8%89&b=%E6%9D%8E%E5%9B%9B"));
+        Assertions.assertEquals("https://www.abc.com?a=%E5%BC%A0%E4%B8%89&b=%E6%9D%8E%E5%9B%9B&c=%E7%8E%8B%E4%BA%94",
+                UriUtils.addQueryParams("https://www.abc.com?c=%E7%8E%8B%E4%BA%94", "a=%E5%BC%A0%E4%B8%89&b=%E6%9D%8E%E5%9B%9B"));
     }
 
 }
