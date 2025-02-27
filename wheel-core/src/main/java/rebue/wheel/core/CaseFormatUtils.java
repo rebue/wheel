2b3c58e24d1aa3ac025a2204af0e26e954c43516
zbz <nnzbz@qq.com>
2025-02-27 16:32:58 +0800

    + 添加字符串处理的常用方法

--- wheel-core/src/main/java/rebue/wheel/core/CaseFormatUtils.java
+++ wheel-core/src/main/java/rebue/wheel/core/CaseFormatUtils.java
@@ -5,8 +5,19 @@ import com.google.common.base.CaseFormat;
 public class CaseFormatUtils {
 
     /**
+     * 分割驼峰格式的字符串为数组
+     * e.g., "splitCamel"->["split","Camel"]
+     *
+     * @param str 小驼峰格式的字符串
+     * @return 烤串格式的字符串
+     */
+    public static String[] splitCamel(final String str) {
+        return str.split("(?=[A-Z])");
+    }
+
+    /**
      * 将小驼峰格式的字符串转换成烤串格式的字符串
-     * e.g., "lower-hyphen".
+     * e.g., "lowerCame" -> "lower-hyphen"
      *
      * @param str 小驼峰格式的字符串
      * @return 烤串格式的字符串
@@ -17,7 +28,8 @@ public class CaseFormatUtils {
 
     /**
      * 将大驼峰格式的字符串转换成烤串格式的字符串
-     *
+     * e.g., "upperCamel" -> "lower-hyphen"
+     * 
      * @param str 大驼峰格式的字符串
      * @return 烤串格式的字符串
      */
@@ -25,4 +37,14 @@ public class CaseFormatUtils {
         return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_HYPHEN, str);
     }
 
+    /**
+     * 将烤串格式的字符串转换成小驼峰格式的字符串
+     * e.g., "lower-hyphen" -> "lowerCamel"
+     *
+     * @param str 小驼峰格式的字符串
+     * @return 烤串格式的字符串
+     */
+    public static String lowerHyphenToLowerCamel(final String str) {
+        return CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, str);
+    }
 }
--- wheel-core/src/main/java/rebue/wheel/core/StrUtils.java
+++ wheel-core/src/main/java/rebue/wheel/core/StrUtils.java
@@ -34,4 +34,17 @@ public class StrUtils {
                 .filter(c -> c == targetChar)
                 .count();
     }
+
+    /**
+     * 首字母大写
+     * 
+     * @param str 要处理的字符串
+     * @return 首字母大写的字符串
+     */
+    public static String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
 }
