3588851736e9173d8d47c68130c7f04303e94d81
zbz <nnzbz@qq.com>
2025-05-30 10:29:21 +0800

    * 升级插件版本为3.5.50

--- wheel-kafka/http-sink-connector/src/main/java/rebue/wheel/kafka/httpsink/HttpSinkCst.java
+++ wheel-kafka/http-sink-connector/src/main/java/rebue/wheel/kafka/httpsink/HttpSinkCst.java
@@ -8,7 +8,7 @@ public interface HttpSinkCst {
     /**
      * Kafka 插件的版本
      */
-    String PLUGIN_VERSION          = "3.5.46";
+    String PLUGIN_VERSION          = "3.5.50";
     /**
      * 配置的默认组
      */
