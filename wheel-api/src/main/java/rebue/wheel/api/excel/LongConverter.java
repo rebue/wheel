32a870a1f8b3668509679a5ebe69ec6b2b4aee53
zbz <nnzbz@qq.com>
2025-07-11 18:33:11 +0800

    * 优化导出代码

---
+++ wheel-api/src/main/java/rebue/wheel/api/excel/LongConverter.java
@@ -0,0 +1,31 @@
+package rebue.wheel.api.excel;
+
+import cn.idev.excel.converters.Converter;
+import cn.idev.excel.enums.CellDataTypeEnum;
+import cn.idev.excel.metadata.GlobalConfiguration;
+import cn.idev.excel.metadata.data.ReadCellData;
+import cn.idev.excel.metadata.data.WriteCellData;
+import cn.idev.excel.metadata.property.ExcelContentProperty;
+
+public class LongConverter implements Converter<Long> {
+    @Override
+    public Class<?> supportJavaTypeKey() {
+        return Long.class;
+    }
+
+    @Override
+    public CellDataTypeEnum supportExcelTypeKey() {
+        return CellDataTypeEnum.STRING;
+    }
+
+    @Override
+    public Long convertToJavaData(ReadCellData<?> cellData, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) {
+        return Long.parseLong(cellData.getStringValue());
+    }
+
+    @Override
+    public WriteCellData<?> convertToExcelData(Long value, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) {
+        return new WriteCellData<>(String.valueOf(value));
+    }
+
+}
---
+++ wheel-core/src/main/java/rebue/wheel/core/spring/ExcelSpringUtils.java
@@ -0,0 +1,49 @@
+package rebue.wheel.core.spring;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import cn.idev.excel.FastExcel;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class ExcelSpringUtils {
+    /**
+     * 下载excel格式的数据
+     *
+     * @param fileName 导出的文件名
+     * @param voClazz  导出数据的视图类
+     * @param dataList 导出的数据列表
+     */
+    public static ResponseEntity<Flux<DataBuffer>> download(String fileName, Class voClazz, List<?> dataList) {
+        // URI编码文件名
+        String                     encodedFileName = URI.create(fileName).toASCIIString();
+        // 设置响应头
+        ResponseEntity.BodyBuilder ok              = ResponseEntity.ok()
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + encodedFileName + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .cacheControl(CacheControl.noCache());
+
+        // 写入数据到Excel文件流中
+        ByteArrayOutputStream      out             = new ByteArrayOutputStream();
+        FastExcel.write(out, voClazz).sheet("Sheet1").doWrite(dataList);
+        byte[] content = out.toByteArray();
+        // 设置内容长度
+        ok.contentLength(content.length);
+        // 将数据写入响应流
+        ByteArrayResource resource = new ByteArrayResource(content);
+        return ok.body(DataBufferUtils.read(resource, new DefaultDataBufferFactory(), content.length).publishOn(Schedulers.boundedElastic()));
+    }
+}
