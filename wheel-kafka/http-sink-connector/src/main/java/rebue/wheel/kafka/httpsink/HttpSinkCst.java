a99dab9e97626bfaa255551b5a9ca224f03c8815
zbz <nnzbz@qq.com>
2025-07-22 14:32:09 +0800

    * 升级robotech的版本

--- wheel-kafka/http-sink-connector/src/main/java/rebue/wheel/kafka/httpsink/HttpSinkCst.java
+++ wheel-kafka/http-sink-connector/src/main/java/rebue/wheel/kafka/httpsink/HttpSinkCst.java
@@ -8,7 +8,7 @@ public interface HttpSinkCst {
     /**
      * Kafka 插件的版本
      */
-    String PLUGIN_VERSION          = "3.7.57";
+    String PLUGIN_VERSION          = "3.7.58";
     /**
      * 配置的默认组
      */
